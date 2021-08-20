164a34b3f1496998895792565b1689fee35e37e4
ka1peshc <kalpesh111997@gmail.com>
2021-08-21 01:57:17 +0530

    Added two more condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,7 @@
 headsCount=0
 tailsCount=0
 count=0
-while [[ $headsCount -lt 21 && $tailsCount -lt 21 ]]
-do
+function flipcoinFunction () {
     result=$((RANDOM%2))
     case $result in
         0)
@@ -10,6 +9,20 @@ do
         1)
             ((tailsCount++));;
     esac
+}
+while [[ $headsCount -lt 21 && $tailsCount -lt 21 ]]
+do
+    flipcoinFunction
     ((count++))
 done
+
+if [ $headsCount -eq $tailsCount ]
+then
+    diff=$(($headsCount - $tailsCount))
+    while [[ diff -gt -3 || diff -lt 2 ]]
+    do
+        flipcoinFunction
+    done
+fi
+
 echo "heads count is "$headsCount "  Tails count is "$tailsCount
