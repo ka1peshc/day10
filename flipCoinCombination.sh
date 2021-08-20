1cef5d97e81707ac76820639468ac6c80995e610
ka1peshc <kalpesh111997@gmail.com>
2021-08-21 02:34:34 +0530

    Printing heads and tails in percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,22 @@
-coin1=$((RANDOM%2))
-if [ $coin1 -eq 0 ]
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi
+#!/bin/bash 
+declare -A singlet
+singlet[Heads]=0
+singlet[Tails]=0
+function coinFlip () {
+    temp=$((RANDOM%2))
+    echo $temp
+}
+i=0
+while [ $i -lt 12 ]
+do
+    coin1=$( coinFlip )
+    if [ $coin1 -eq 0 ]
+    then
+        singlet[Heads]=$(( ${singlet[Heads]} + 1 ))
+    else
+        singlet[Tails]=$(( ${singlet[Tails]} + 1 ))
+    fi
+    ((i++))
+done
+echo "Heads " $(($((${singlet[Heads]}*100))/12))"%"
+echo "Tails " $(($((${singlet[Tails]}*100))/12))"%"
