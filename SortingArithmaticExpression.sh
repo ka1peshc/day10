4862ccbb0fa1abd3db491d73c94e593bdac4a388
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 19:29:19 +0530

    Stored result in dictionary format

--- SortingArithmaticExpression.sh
+++ SortingArithmaticExpression.sh
@@ -2,11 +2,20 @@ read -p "Enter number for a " a
 read -p "Enter number for b " b
 read -p "Enter number for c " c
 
+declare -A resultDict
+
 expr1=$(( $a + $(($b * $c )) ))
+resultDict[expr1]=$expr1
 echo $expr1
 expr2=$(( $(( $a * $b )) + $c ))
+resultDict[expr2]=$expr2
 echo $expr2
 expr3=$(( $c + $(( $a / $b )) ))
+resultDict[expr3]=$expr3
 echo $expr3
 expr4=$(( $(( $a % $b )) + $c ))
+resultDict[expr4]=$expr4
 echo $expr4
+
+echo "${!resultDict[@]}"
+echo "${resultDict[@]}"
