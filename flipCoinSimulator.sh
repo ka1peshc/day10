2b9c80c53a681bc554a933fea89dca87425c284e
ka1peshc <kalpesh111997@gmail.com>
2021-08-21 01:29:35 +0530

    Loop n times and keep count of win respectively

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,15 @@
-result=$((RANDOM%2))
-case $result in
-    0)
-        echo "Head";;
-    1)
-        echo "Tails";;
-esac
+headsCount=0
+tailsCount=0
+count=0
+while [ $count -lt 16 ]
+do
+    result=$((RANDOM%2))
+    case $result in
+        0)
+            ((headsCount++));;
+        1)
+            ((tailsCount++));;
+    esac
+    ((count++))
+done
+echo "heads count is "$headsCount "  Tails count is "$tailsCount
